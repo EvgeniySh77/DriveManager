65a9931d7daaefb702d11e121492fd2927710086
EvgeniySh77 <evgen.sh77@mail.ru>
2022-02-20 19:07:54 +0300

    Добавлен метод CountNumOfFoldersAndFiles() для подсчета папок и файлов

--- Program.cs
+++ Program.cs
@@ -24,6 +24,51 @@ namespace DriveManager
             }
 
             Console.ReadKey();
+            Console.WriteLine(Environment.NewLine);
+
+            GetCatalogs(); //   Вызов метода получения директорий
+
+            Console.ReadKey();
+            Console.WriteLine(Environment.NewLine);
+
+            CountNumOfFoldersAndFiles(); //   Вызов метода подчета количества папок и файлов
+
+            Console.ReadKey();
+        }
+
+        static void CountNumOfFoldersAndFiles()
+        {
+            try
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(@"C:\");
+                if (dirInfo.Exists)
+                {
+                    Console.WriteLine(dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        static void GetCatalogs()
+        {
+            string dirName = @"C:\"; // Прописываем путь к корневой директории MacOS (для Windows скорее всего тут будет "C:\\")
+            if (Directory.Exists(dirName)) // Проверим, что директория существует
+            {
+                Console.WriteLine("Папки:");
+                string[] dirs = Directory.GetDirectories(dirName);  // Получим все директории корневого каталога
+
+                foreach (string d in dirs) // Выведем их все
+                    Console.WriteLine(d);
+
+                Console.WriteLine();
+                Console.WriteLine("Файлы:");
+                string[] files = Directory.GetFiles(dirName);// Получим все файлы корневого каталога
+
+                foreach (string s in files)   // Выведем их все
+                    Console.WriteLine(s);
+            }
         }
     }
 }
